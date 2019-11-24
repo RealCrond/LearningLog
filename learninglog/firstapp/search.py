378fa1f791352be12ed2aff5b279a50bae1a44fb
Herman <1261287592@qq.com>
2019-11-24 22:25:30 +0800

    Post example

---
+++ learninglog/firstapp/search.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.views.decorators import csrf
+
+def search(request):
+    return render(request,"post.html")
+
+def search_post(request):
+    ctx = {}
+    if request.POST:
+        ctx['rlt'] = request.POST['q']
+    return render(request,"post.html",ctx)
--- learninglog/firstapp/urls.py
+++ learninglog/firstapp/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url,include
 from . import views
+from . import search
 
 urlpatterns = [
     #url(r'^$', views.main),
@@ -7,5 +8,7 @@ urlpatterns = [
     url(r'^$', views.base, name='index'),
     url(r'^index$', views.index),
     url(r'^topics/$', views.topics, name='topics'),
-    url(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic')
+    url(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic'),
+    url(r'^search$',search.search,name='search'),
+    url(r'^search-post$',search.search_post,name='search_post'),
 ]
